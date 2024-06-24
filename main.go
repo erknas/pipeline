7cb57cc9efb161e8fefd5196f26499d23e0103c5
Alexandr <vin89441@gmail.com>
2024-06-24 16:34:57 +0300

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// gen converts list of integers into channel
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, num := range nums {
+			out <- num
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+// sq recieve integers from channel and returns channel
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for num := range in {
+			out <- num * num
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func main() {
+	for num := range sq(gen(1, 2, 3, 4, 5, 6, 7, 8, 9)) {
+		fmt.Println(num)
+	}
+}
